e271470ed50e66bc909f0a7f79a16c82e2745ded
butyalena <butyalena@gmail.com>
2023-03-04 22:04:22 +0300

    Implement task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;  
+public class Program  
+{  
+    public static void Main()  
+{  
+    string[] arr = new string[5]; 
+
+    Console.Write("\n\nRead and Print elements of an array:\n");
+    Console.Write("-----------------------------------------\n");	
+  
+    Console.Write("Input 5 elements in the array :\n");
+
+    int i;
+
+    for(i=0; i<5; i++)  
+    {  
+	    Console.Write("element - {0} : ",i);
+	    arr[i] = Console.ReadLine();  		
+    }  
+  
+    Console.Write("[ " + string.Join(", ", arr.Where(c => c.Length <= 3).ToArray()) + " ]");
+  }
+}
